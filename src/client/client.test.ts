865e54430c9d3459151a07772816cb222ea4da45
NachoTaD <adolfo@thinkanddev.com>
2024-06-14 01:39:48 -0300

    fix: client test refactoring

--- src/client/client.test.ts
+++ src/client/client.test.ts
@@ -12,11 +12,13 @@ import { CreateGameReducer } from '../core/reducer';
 import { InitializeGame } from '../core/initialize';
 import { Client, createMoveDispatchers } from './client';
 import { ProcessGameConfig } from '../core/game';
-import { DummyTransport } from './transport/dummy';
+import { DummyTransport, DummyImpl } from './transport/dummy';
 import { CartesifyTransport } from './transport/cartesify-transport';
-import type { Transport } from './transport/transport';
+import type { TransportOpts } from './transport/transport';
+import { Transport } from './transport/transport';
 import { LocalTransport, Local } from './transport/local';
-import { SocketIO } from './transport/socketio';
+import { SocketIOTransport, SocketIO } from './transport/socketio';
+
 import {
   update,
   sync,
@@ -140,14 +142,131 @@ test('isActive', () => {
   expect(client.getState().isActive).toBe(false);
 });
 
+describe('multiplayer transports', () => {
+  test('default transport', () => {
+    const client = Client({
+      game: {
+        moves: {
+          A: (_, arg) => ({ arg }),
+        },
+      },
+    });
+    expect((client as any).transport).toBeInstanceOf(DummyImpl);
+  });
+
+  test('CartesifyTransport', () => {
+    const client = Client({
+      game: {
+        moves: {
+          A: (_, arg) => ({ arg }),
+        },
+      },
+      multiplayer: () =>
+        new CartesifyTransport({
+          url: 'http://localhost:5004',
+        } as unknown as TransportOpts),
+    });
+    expect((client as any).transport).toBeInstanceOf(CartesifyTransport);
+  });
+
+  test('custom transport function', () => {
+    const customTransport = jest.fn();
+    const client = Client({
+      game: {
+        moves: {
+          A: (_, arg) => ({ arg }),
+        },
+      },
+      multiplayer: customTransport,
+    });
+    expect(customTransport).toHaveBeenCalled();
+  });
+
+  test('invalid multiplayer value', () => {
+    expect(() => {
+      Client({
+        game: {
+          moves: {
+            A: (_, arg) => ({ arg }),
+          },
+        },
+        multiplayer: 'invalid',
+      });
+    }).toThrow('Invalid multiplayer option');
+  });
+});
+
 describe('multiplayer', () => {
-  describe('CartesifyTransport', () => {
+  describe('socket.io master', () => {
+    const host = 'host';
+    const port = '4321';
+    let client;
+
+    beforeAll(() => {
+      client = Client({
+        game: { moves: { A: () => {}, Invalid: () => INVALID_MOVE } },
+        multiplayer: SocketIO({ server: host + ':' + port }),
+      });
+      client.start();
+    });
+
+    afterAll(() => {
+      client.stop();
+      jest.restoreAllMocks();
+    });
+
+    test('correct transport used', () => {
+      expect(client.transport instanceof SocketIOTransport).toBe(true);
+    });
+
+    test('server set when provided', () => {
+      expect(client.transport.socket.io.engine.hostname).toEqual(host);
+      expect(client.transport.socket.io.engine.port).toEqual(port);
+    });
+
+    test('onAction called', () => {
+      jest.spyOn(client.transport, 'sendAction');
+      const state = { G: {}, ctx: { phase: '' }, plugins: {} };
+      const filteredMetadata = [];
+      client.store.dispatch(sync({ state, filteredMetadata } as SyncInfo));
+      client.moves.A();
+      expect(client.transport.sendAction).toHaveBeenCalled();
+    });
+
+    test('strip transients action not sent to transport', () => {
+      jest.spyOn(client.transport, 'sendAction');
+      const state = { G: {}, ctx: { phase: '' }, plugins: {} };
+      const filteredMetadata = [];
+      client.store.dispatch(sync({ state, filteredMetadata } as SyncInfo));
+      client.moves.Invalid();
+      expect(client.transport.sendAction).not.toHaveBeenCalledWith(
+        expect.any(Object),
+        { type: Actions.STRIP_TRANSIENTS }
+      );
+    });
+
+    test('Sends and receives chat messages', () => {
+      jest.spyOn(client.transport, 'sendAction');
+      client.updatePlayerID('0');
+      client.updateMatchID('matchID');
+      jest.spyOn(client.transport, 'sendChatMessage');
+
+      client.sendChatMessage({ message: 'foo' });
+
+      expect(client.transport.sendChatMessage).toHaveBeenCalledWith(
+        'matchID',
+        expect.objectContaining({ payload: { message: 'foo' }, sender: '0' })
+      );
+    });
+  });
+
+  describe('multiplayer: SocketIO()', () => {
     let client;
 
     beforeAll(() => {
       client = Client({
         game: {},
-        multiplayer: 'cartesify',
+        multiplayer: SocketIO(),
       });
       client.start();
     });
@@ -156,189 +275,112 @@ describe('multiplayer', () => {
       client.stop();
     });
 
-    test('CartesifyTransport is used', () => {
-      expect(client.transport instanceof CartesifyTransport).toBe(true);
-    });
-
-    describe('local master', () => {
-      let client0;
-      let client1;
-      let spec;
-
-      beforeAll(() => {
-        spec = {
-          game: { moves: { A: ({ playerID }) => ({ A: playerID }) } },
-          multiplayer: Local(),
-        };
-
-        client0 = Client({ ...spec, playerID: '0' });
-        client1 = Client({ ...spec, playerID: '1' });
-      });
-
-      afterAll(() => {
-        client0.stop();
-        client1.stop();
-      });
-
-      test('correct transport used', () => {
-        expect(client0.transport instanceof LocalTransport).toBe(true);
-        expect(client1.transport instanceof LocalTransport).toBe(true);
-      });
-
-      test('multiplayer interactions', () => {
-        expect(client0.getState().ctx.currentPlayer).toBe('0');
-        expect(client1.getState().ctx.currentPlayer).toBe('0');
-
-        client0.moves.A();
-
-        expect(client0.getState().G).toEqual({ A: '0' });
-        expect(client1.getState().G).toEqual({ A: '0' });
-
-        client0.events.endTurn();
-
-        expect(client0.getState().ctx.currentPlayer).toBe('1');
-        expect(client1.getState().ctx.currentPlayer).toBe('1');
-
-        client1.moves.A();
-
-        expect(client0.getState().G).toEqual({ A: '1' });
-        expect(client1.getState().G).toEqual({ A: '1' });
-
-        client0.sendChatMessage({ message: 'foo' });
-
-        expect(client0.chatMessages).toEqual([
-          expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
-        ]);
-        expect(client1.chatMessages).toEqual([
-          expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
-        ]);
-      });
-
-      test('send', async () => {
-        await expect(
-          Promise.all([
-            new Promise<void>((resolve) =>
-              client0.transport.subscribe(
-                ({ type }) => type === 'chat' && resolve()
-              )
-            ),
-            client0.send({ type: 'bar', payload: { message: 'bar' } }),
-          ])
-        ).resolves.toBeUndefined();
-      });
-
-      test('reconnect with state updates', () => {
-        client0.disconnect();
-        client0.connect();
-
-        client0.moves.A();
-
-        expect(client0.getState().G).toEqual({ A: '1' });
-        expect(client1.getState().G).toEqual({ A: '1' });
-      });
+    test('correct transport used', () => {
+      expect(client.transport instanceof SocketIOTransport).toBe(true);
     });
   });
-});
 
-describe('createMoveDispatchers', () => {
-  const Game = {
-    moves: {
-      A: (G) => G,
-      B: () => INVALID_MOVE,
-    },
-  };
+  describe('local master', () => {
+    let client0;
+    let client1;
+    let spec;
 
-  const reducer = CreateGameReducer({ game: Game });
+    beforeAll(() => {
+      spec = {
+        game: { moves: { A: ({ playerID }) => ({ A: playerID }) } },
+        multiplayer: Local(),
+      };
 
-  const store = createStore(reducer, InitializeGame({ game: Game }));
+      client0 = Client({ ...spec, playerID: '0' });
+      client1 = Client({ ...spec, playerID: '1' });
 
-  const m = createMoveDispatchers(store, Game);
+      client0.start();
+      client1.start();
+    });
 
-  test('move dispatcher', () => {
-    expect(typeof m.A).toBe('function');
-    expect(typeof m.B).toBe('function');
+    afterAll(() => {
+      client0.stop();
+      client1.stop();
+    });
+
+    test('correct transport used', () => {
+      expect(client0.transport instanceof LocalTransport).toBe(true);
+      expect(client1.transport instanceof LocalTransport).toBe(true);
+    });
+
+    test('multiplayer interactions', () => {
+      expect(client0.getState().ctx.currentPlayer).toBe('0');
+      expect(client1.getState().ctx.currentPlayer).toBe('0');
+
+      client0.moves.A();
+
+      expect(client0.getState().G).toEqual({ A: '0' });
+      expect(client1.getState().G).toEqual({ A: '0' });
+
+      client0.events.endTurn();
+
+      expect(client0.getState().ctx.currentPlayer).toBe('1');
+      expect(client1.getState().ctx.currentPlayer).toBe('1');
+
+      client1.moves.A();
+
+      expect(client0.getState().G).toEqual({ A: '1' });
+      expect(client1.getState().G).toEqual({ A: '1' });
+
+      client0.sendChatMessage({ message: 'foo' });
+
+      expect(client0.chatMessages).toEqual([
+        expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
+      ]);
+      expect(client1.chatMessages).toEqual([
+        expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
+      ]);
+
+      client1.sendChatMessage({ message: 'bar' });
+      expect(client0.chatMessages).toEqual([
+        expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
+        expect.objectContaining({ sender: '1', payload: { message: 'bar' } }),
+      ]);
+      expect(client1.chatMessages).toEqual([
+        expect.objectContaining({ sender: '0', payload: { message: 'foo' } }),
+        expect.objectContaining({ sender: '1', payload: { message: 'bar' } }),
+      ]);
+    });
   });
 
-  test('move execution', () => {
-    const state = m.A();
-    expect(state).not.toBe(undefined);
-  });
+  describe('custom transport', () => {
+    class CustomTransport extends Transport {
+      connect() {}
+      disconnect() {}
+      sendAction() {}
+      sendChatMessage() {}
+      requestSync() {}
+      updateMatchID() {}
+      updatePlayerID() {}
+      updateCredentials() {}
+      setMetadata(metadata) {
+        this.notifyClient({ type: 'matchData', args: ['default', metadata] });
+      }
+    }
+    const customTransport = (opts) => new CustomTransport(opts);
 
-  test('invalid move', () => {
-    const state = m.B();
-    expect(state).toBe(undefined);
-  });
-});
+    let client;
 
-describe('plugins', () => {
-  let client: ReturnType<typeof Client>;
-  const plugin = {
-    name: 'plugin',
-    api: () => ({
-      test: () => 'test',
-    }),
-    flush: () => null,
-    setup: () => ({}),
-  };
+    beforeAll(() => {
+      client = Client({
+        game: { moves: { A: () => {} } },
+        multiplayer: customTransport,
+      });
+    });
 
-  const game: Game = {
-    setup: () => ({}),
-    plugins: [plugin],
-  };
+    test('correct transport used', () => {
+      expect(client.transport).toBeInstanceOf(CustomTransport);
+    });
 
-  beforeAll(() => {
-    client = Client({ game });
-  });
-
-  test('initialize plugin', () => {
-    expect(client.getState().plugins.plugin).toEqual({});
-  });
-
-  test('api', () => {
-    expect(client.getState().plugins.plugin?.api().test()).toEqual('test');
-  });
-});
-
-describe('Authentication and Reconnection', () => {
-  let client;
-  const game: Game = {
-    moves: { A: (_, arg) => ({ arg }) },
-  };
-
-  beforeEach(() => {
-    client = Client({ game, multiplayer: SocketIO() });
-  });
-
-  afterEach(() => {
-    client.stop();
-  });
-
-  test('update credentials', () => {
-    client.updateCredentials('new-credentials');
-    expect(client.credentials).toBe('new-credentials');
-  });
-
-  test('credentials included in actions', () => {
-    jest.spyOn(client.transport, 'sendAction');
-    client.updateCredentials('new-credentials');
-    client.moves.A(42);
-    expect(client.transport.sendAction).toHaveBeenCalledWith(
-      expect.objectContaining({
-        credentials: 'new-credentials',
-      }),
-      expect.anything()
-    );
-  });
-
-  test('automatic reconnection', () => {
-    return new Promise<void>((done) => {
-      jest.spyOn(client.transport, 'connect');
-      client.start();
-      client.transport.socket.emit('disconnect');
-      setTimeout(() => {
-        expect(client.transport.connect).toHaveBeenCalledTimes(2);
-        done();
-      }, 1000);
+    test('metadata callback', () => {
+      const metadata = { m: true };
+      client.transport.setMetadata(metadata);
+      expect(client.matchData).toEqual(metadata);
     });
   });
 });
@@ -464,8 +506,10 @@ describe('strip secret only on server', () => {
         setup: () => initial,
         playerView: ({ G }) => {
           const r = { ...G };
-          r.sum = r.secret.reduce((prev, curr) => prev + curr);
-          delete r.secret;
+          if (r.secret) {
+            r.sum = r.secret.reduce((prev, curr) => prev + curr, 0);
+            delete r.secret;
+          }
           return r;
         },
         moves: { A: ({ playerID }) => ({ A: playerID }) },
@@ -910,9 +954,9 @@ describe('subscribe', () => {
         game: {},
         multiplayer: Local(),
       });
-      client.subscribe(fn);
-      expect(fn).not.toBeCalled();
       client.start();
+      expect(fn).not.toBeCalled();
+      client.subscribe(fn);
       expect(fn).toBeCalled();
       client.stop();
     });
