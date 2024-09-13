4927fcd8a198a2bb8afab1d0667515a553cd4f46
NachoTaD <adolfo@thinkanddev.com>
2024-09-13 10:25:22 -0300

    chore: remove unnecessary comments and revert to default

--- examples/react-web/server.js
+++ examples/react-web/server.js
@@ -6,39 +6,15 @@
  * https://opensource.org/licenses/MIT.
  */
 
-import { Server } from '@think-and-dev/cartesi-boardgame/server';
+import { Server, Origins } from 'boardgame.io/server';
 import TicTacToe from './src/tic-tac-toe/game';
-import cors from 'cors';
-
-console.log('Starting server with custom configuration');
+import Chess from './src/chess/game';
 
 const PORT = process.env.PORT || 8000;
-
 const server = Server({
-  games: [TicTacToe],
-  origins: ['http://localhost:1234', 'http://localhost:3000'],
+  games: [TicTacToe, Chess],
+  origins: [Origins.LOCALHOST],
 });
-
-server.app.use(
-  cors({
-    // origin: ['http://localhost:1234', 'http://localhost:3000'],
-    origin: '*',
-    // methods: ['GET', 'POST'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-
-server.app.use(async (ctx, next) => {
-  console.log(`Received request: ${ctx.method} ${ctx.url}`);
-  if (ctx.path === '/games') {
-    ctx.body = ['TicTacToe'];
-    console.log('Games endpoint hit, returning:', ctx.body);
-  } else {
-    await next();
-  }
-});
-
 server.run(PORT, () => {
   console.log(`Serving at: http://localhost:${PORT}`);
 });
