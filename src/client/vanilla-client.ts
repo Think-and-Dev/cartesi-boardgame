48037d7ee4b99e54784fccc942c4081f89308187
ErikaFogar <erikafogar.j@gmail.com>
2024-11-18 11:19:35 +0100

    Working around: board rendering in vanilla-client.ts

--- src/client/vanilla-client.ts
+++ src/client/vanilla-client.ts
@@ -1,47 +1,124 @@
+/*
+ * Copyright 2017 The boardgame.io Authors
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 import { Client as RawClient } from './client';
 import type { ClientOpts, ClientState, _ClientImpl } from './client';
-import { Board } from '../../examples/typescript/src/Board2';
-import Debug from './debug/Debug.svelte';
 
-interface VanillaClientOpts<G> extends ClientOpts<G> {
-  rootElement: HTMLElement;
-  board?: (opts: any) => HTMLElement | Board;
-}
+type WrappedBoardDelegates = 'matchID' | 'playerID' | 'credentials';
 
-export class VanillaClient<G> {
-  private client: _ClientImpl<G>;
-  private rootElement: HTMLElement;
-  private boardElement?: HTMLElement | Board;
-  private debug: Debug | null = null;
-  constructor(opts: VanillaClientOpts<G>) {
-    const {
-      game,
-      numPlayers,
-      multiplayer,
-      debug,
-      enhancer,
-      rootElement,
-      matchID,
-      playerID,
-      credentials,
-      board,
-    } = opts;
-    this.rootElement = rootElement;
+export type WrappedBoardProps = Pick<
+  ClientOpts,
+  WrappedBoardDelegates | 'debug'
+>;
 
-    this.client = RawClient({
-      game,
-      numPlayers,
-      multiplayer,
-      debug,
-      enhancer,
-      matchID,
-      playerID,
-      credentials,
-    });
+type ExposedClientProps<G extends any = any> = Pick<
+  _ClientImpl<G>,
+  | 'log'
+  | 'moves'
+  | 'events'
+  | 'reset'
+  | 'undo'
+  | 'redo'
+  | 'playerID'
+  | 'matchID'
+  | 'matchData'
+  | 'sendChatMessage'
+  | 'chatMessages'
+>;
 
-    if (board) {
-      if (typeof board === 'function') {
-        this.boardElement = board({
+export type BoardProps<G extends any = any> = ClientState<G> &
+  Omit<WrappedBoardProps, keyof ExposedClientProps<G>> &
+  ExposedClientProps<G> & {
+    isMultiplayer: boolean;
+  };
+
+type VanillaClientOpts<
+  G extends any = any,
+  P extends BoardProps<G> = BoardProps<G>,
+  PluginAPIs extends Record<string, unknown> = Record<string, unknown>
+> = Omit<ClientOpts<G, PluginAPIs>, WrappedBoardDelegates> & {
+  board?: (props: P) => void; // Cambiado a función pura
+  loading?: () => void; // Cambiado a función pura
+};
+
+export function Client<
+  G extends any = any,
+  P extends BoardProps<G> = BoardProps<G>,
+  PluginAPIs extends Record<string, unknown> = Record<string, unknown>
+>(opts: VanillaClientOpts<G, P, PluginAPIs>) {
+  const { game, numPlayers, board, multiplayer, enhancer } = opts;
+  let { loading, debug } = opts;
+
+  if (loading === undefined) {
+    loading = () => console.log('connecting...'); // Función de carga simple
+  }
+
+  type AdditionalProps = Omit<P, keyof BoardProps<G>>;
+
+  return class WrappedBoard {
+    client: _ClientImpl<G>;
+    unsubscribe?: () => void;
+    props: WrappedBoardProps & AdditionalProps;
+
+    constructor(props: WrappedBoardProps & AdditionalProps) {
+      this.props = props;
+
+      if (debug === undefined) {
+        debug = props.debug;
+      }
+
+      this.client = RawClient({
+        game,
+        debug,
+        numPlayers,
+        multiplayer,
+        matchID: props.matchID,
+        playerID: props.playerID,
+        credentials: props.credentials,
+        enhancer,
+      });
+    }
+
+    componentDidMount() {
+      this.unsubscribe = this.client.subscribe(() => this.update());
+      this.client.start();
+    }
+
+    componentWillUnmount() {
+      this.client.stop();
+      this.unsubscribe?.();
+    }
+
+    componentDidUpdate(prevProps: WrappedBoardProps & AdditionalProps) {
+      if (this.props.matchID !== prevProps.matchID) {
+        this.client.updateMatchID(this.props.matchID);
+      }
+      if (this.props.playerID !== prevProps.playerID) {
+        this.client.updatePlayerID(this.props.playerID);
+      }
+      if (this.props.credentials !== prevProps.credentials) {
+        this.client.updateCredentials(this.props.credentials);
+      }
+    }
+
+    update() {
+      const state = this.client.getState();
+
+      if (state === null) {
+        loading();
+        return;
+      }
+
+      if (board) {
+        board({
+          ...state,
+          ...(this.props as P),
+          isMultiplayer: !!multiplayer,
           moves: this.client.moves,
           events: this.client.events,
           matchID: this.client.matchID,
@@ -53,75 +130,8 @@ export class VanillaClient<G> {
           matchData: this.client.matchData,
           sendChatMessage: this.client.sendChatMessage,
           chatMessages: this.client.chatMessages,
-          isMultiplayer: !!this.client.multiplayer,
         });
-      } else {
-        this.boardElement = board;
       }
-      this.rootElement.appendChild(this.boardElement as HTMLElement);
     }
-
-    if (debug !== false) {
-      const debugContainer = document.createElement('div');
-      this.rootElement.appendChild(debugContainer);
-      this.debug = new Debug({
-        target: debugContainer,
-        props: { clientManager: this },
-      });
-    }
-
-    this.client.subscribe(() => this.update());
-  }
-
-  public destroy() {
-    this.client.stop();
-  }
-
-  private update() {
-    const state = this.client.getState();
-    if (state && this.boardElement instanceof Board) {
-      this.boardElement.update(state);
-    } else {
-      this.renderBoard(state);
-    }
-  }
-
-  private renderBoard(state: ClientState<G>) {
-    if (!this.boardElement || !(this.boardElement instanceof HTMLElement))
-      return;
-
-    this.boardElement.innerHTML = '';
-
-    const table = document.createElement('table');
-    for (let row = 0; row < 3; row++) {
-      const tr = document.createElement('tr');
-      for (let col = 0; col < 3; col++) {
-        const cell = document.createElement('td');
-        const cellIndex = row * 3 + col;
-        const cellValue = state.G.cells[cellIndex]; // 'X', 'O' o null
-
-        cell.textContent = cellValue ? cellValue : '';
-
-        if (state.isActive) {
-          cell.addEventListener('click', () => {
-            this.client.moves.clickCell(cellIndex);
-          });
-        }
-
-        tr.appendChild(cell);
-      }
-      table.appendChild(tr);
-    }
-
-    // Añadir el tablero actualizado al boardElement
-    this.boardElement.appendChild(table);
-  }
-
-  public start() {
-    this.client.start();
-  }
-
-  public stop() {
-    this.client.stop();
-  }
+  };
 }
