07339f2df7169140e7188bb36c14ec485cd364ce
ErikaFogar <erikafogar.j@gmail.com>
2024-11-25 15:30:47 +0100

    Modify server to add lobbyConfig for running lobby on a separate server

--- examples/backend/app.ts
+++ examples/backend/app.ts
@@ -2,13 +2,34 @@ import { Server, Sqlite } from '@think-and-dev/cartesi-boardgame/server';
 import { TicTacToe } from './Game';
 
 const database = new Sqlite();
+
 async function main() {
+  // Configuración del servidor con lobby separado
   const server = Server({
     games: [TicTacToe],
     db: database,
     origins: ['http://localhost:1234'],
   });
 
-  server.run(8000);
+  const GAME_PORT = 8000; // Puerto para el backend del juego
+  const LOBBY_PORT = 9000; // Puerto para el lobby separado
+
+  // Ejecutar el servidor con la configuración del Lobby
+  server.run(
+    {
+      port: GAME_PORT, // Puerto para el backend del juego
+      lobbyConfig: {
+        apiPort: LOBBY_PORT, // Puerto para el lobby
+        apiCallback: () =>
+          console.log(`Lobby corriendo en http://localhost:${LOBBY_PORT}`),
+      },
+    },
+    () => {
+      console.log(
+        `Backend del juego corriendo en http://localhost:${GAME_PORT}`
+      );
+    }
+  );
 }
+
 main().catch(console.error);
