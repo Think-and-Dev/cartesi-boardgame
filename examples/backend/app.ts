dae96167558ee8d374029e086652d1c13c7adde7
ErikaFogar <erikafogar.j@gmail.com>
2024-12-03 21:34:55 +0100

    Added Cors configurations

--- examples/backend/app.ts
+++ examples/backend/app.ts
@@ -1,14 +1,26 @@
 import { Server, Sqlite } from '@think-and-dev/cartesi-boardgame/server';
 import { TicTacToe } from './Game';
+import cors from '@koa/cors';
 
 const database = new Sqlite();
 async function main() {
   const server = Server({
     games: [TicTacToe],
     db: database,
-    origins: ['http://localhost:1234'],
+    origins: ['http://localhost:1234', 'http://localhost:3000'],
   });
+  console.log('Server running on port 8000');
+
+  server.app.use(
+    cors({
+      origin: '*',
+      allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowHeaders: ['Content-Type', 'Authorization'],
+    })
+  );
 
   server.run(8000);
+
+  console.log('confirmadisimo');
 }
 main().catch(console.error);
