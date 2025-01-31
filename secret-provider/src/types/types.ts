53c5814594a45b5ff774f5efa0b8d40f220577bc
ErikaFogar <erikafogar.j@gmail.com>
2025-01-31 11:14:45 +0100

    feat(secret-provider): add types for shuffled deck and hashes

--- secret-provider/src/types/types.ts
+++ secret-provider/src/types/types.ts
@@ -3,7 +3,22 @@
  * - StoredSecret: Structure for stored secrets
  * - Includes original value and salt used
  */
-export type StoredSecret = {
+export interface StoredSecret {
   originalValue: unknown; // The value we want to hide
   saltUsed: string; // Random value we added for security
-};
+}
+
+// Cartesi
+export interface ShuffledHashesPayload {
+  hashes: Array<{
+    key: string;
+    hash: string;
+  }>;
+}
+
+export interface ShuffledDeck {
+  hashes: Array<{
+    key: string;
+    hash: string;
+  }>;
+}
